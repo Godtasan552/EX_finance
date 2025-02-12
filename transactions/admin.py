31e55ea90747e987c3be47872bcfbc3e56b00737
Weerawat Tammung <werawat7903@gmail.com>
2025-02-12 23:32:40 +0700

    set position home and decorate color and fornt

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Transaction
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'amount', 'type', 'category')
+    list_filter = ('type', 'category', 'date')
+    search_fields = ('user__username', 'amount', 'description')
